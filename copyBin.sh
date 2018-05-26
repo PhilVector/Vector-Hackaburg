07e53fa2cccff4d20802007fdd1294aad47fb002
hyp <botmayank@gmail.com>
2018-05-26 12:51:26 +0200

    Add function to copy src

---
+++ copyBin.sh
@@ -0,0 +1,3 @@
+#/!bin/sh
+#pscp HackaburgProject/Debug/HackaburgProject nvidia@tegra-ubuntu.local:/home/nvidia/VectorPhilipp
+pscp HackaburgProject/Debug/HackaburgProject nvidia@tegra-ubuntu.local:/home/nvidia/VectorPhilipp
